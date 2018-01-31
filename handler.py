9f408df5e45ca8984075a4bc04a768b42e6e9f40
mixnota <mixnota@yandex.ru>
2018-01-31 17:21:29 +0300

    Create handler.py

---
+++ handler.py
@@ -0,0 +1,33 @@
+all_messages = []
+
+def handle_message(message, nickname="user"):
+    '''handles message:
+    @message - text of recieved message
+    @nickname - nickname of sender
+
+    @returns - text of response
+    '''
+    global all_messages
+
+    message_split = message.split()
+
+    answer = "no command found"
+    if message == "\show":
+        answer = all_messages
+    elif message_split[0] == "\write":
+        answer = ' '.join(message_split[1:])
+        all_messages.append(answer)
+    return answer
+
+
+if __name__ == "__main__":
+    # dirty python magic, will talk about on the next lesson
+    # just ignore for now
+
+    nick = input("Enter your nickname: ")
+
+    while True:
+        msg = input("Your message: ")
+        ans = handle_message(msg, nick)
+
+        print(ans)
