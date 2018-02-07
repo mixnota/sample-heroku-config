039aa2edb1e4de8ffaa5952e9dc02806ed8e0d15
mixnota <mixnota@gmail.com>
2018-02-07 17:04:40 +0300

    ge

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import requests
 from handler import handle_message
 
 while True:
-    res = requests.get("")
+    res = requests.get("https://api.telegram.org/boted8953b0bd55fcc772f52337d3586c54052198feccdf89306b872128a3bcb895/getUpdates")
     d = res.json()
 
     for elem in d["result"]:
