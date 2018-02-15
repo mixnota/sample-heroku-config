9c5bc7691939d23fee62e3f0845668985bfb2812
mixnota <mixnota@yandex.ru>
2018-02-16 02:04:01 +0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -10,18 +10,18 @@ def handle_message(message, nickname="user"):
     global all_messages
 
     message_split = message.split()
-
     answer = "no command found"
-    if message == "\show":
+    if message == "/show":
         answer = ""
         for message in all_messages:
             answer += message + '\n'
-    elif message_split[0] == "\write":
-        answer = ' '.join(message_split[1:])
-        all_messages.append(answer)
+    else:
+        all_messages.append(message)
+        return message
     return answer
 
 
+'''
 if __name__ == "__main__":
     # dirty python magic, will talk about on the next lesson
     # just ignore for now
@@ -33,3 +33,4 @@ if __name__ == "__main__":
         ans = handle_message(msg, nick)
 
         print(ans)
+'''
